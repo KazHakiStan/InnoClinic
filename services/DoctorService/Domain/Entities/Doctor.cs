f8fe50cec11b886f27a2f688c3e9eb3239928496
Danila Zakharchuk <zakharchukdanila@gmail.com>
2025-10-21 14:50:46 +0200

    Add AccountId FK to Doctor model

--- services/DoctorService/Domain/Entities/Doctor.cs
+++ services/DoctorService/Domain/Entities/Doctor.cs
@@ -7,6 +7,7 @@ public class Doctor
     public string LastName { get; set; } = null!;
     public string? MiddleName { get; set; }
     public DateTime DateOfBirth { get; set; }
+    public Guid AccountId { get; set; }
     public int CareerStartYear { get; set; }
     public string Status { get; set; } = null!;
 }
--- services/DoctorService/Infrastructure/Data/Migrations/20251021124929_AddAccountIdDoctor.Designer.cs
+++ services/DoctorService/Infrastructure/Data/Migrations/20251021124929_AddAccountIdDoctor.Designer.cs
@@ -4,16 +4,19 @@ using DoctorService.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace DoctorService.Migrations
+namespace DoctorService.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(DoctorDbContext))]
-    partial class DoctorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251021124929_AddAccountIdDoctor")]
+    partial class AddAccountIdDoctor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
