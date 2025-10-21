e52f6d86c43beb5777c57ee8cd863e70649798bd
Danila Zakharchuk <zakharchukdanila@gmail.com>
2025-10-21 15:31:11 +0200

    Add auth service to doctor

--- services/DoctorService/Infrastructure/Extensions/WebApplicationExtensions.cs
+++ services/DoctorService/Infrastructure/Extensions/WebApplicationExtensions.cs
@@ -1,3 +1,5 @@
+using DoctorService.Infrastructure.Middleware;
+
 namespace DoctorService.Infrastructure.Extensions;
 
 public static class WebApplicationExtensions
@@ -11,6 +13,7 @@ public static class WebApplicationExtensions
         }
 
         app.UseHttpsRedirection();
+        app.UseMiddleware<AuthValidationMiddleware>();
         app.UseAuthorization();
         app.MapControllers();
 
