e22fc0dc3f54f9fd2812dd671ae2d3f0232bc722
Danila Zakharchuk <zakharchukdanila@gmail.com>
2025-10-27 10:56:36 +0100

    feat: add auth to account service

--- services/AccountService/Program.cs
+++ services/AccountService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using AccountService.Infrastructure.Data;
 using AccountService.Infrastructure.Extensions;
+using AccountService.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,12 @@ builder.Services.AddApiServices();
 
 builder.Services.AddDbContext<AccountDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.Configure<AuthServiceOptions>(
+    builder.Configuration.GetSection("AuthService")
+    );
+
+builder.Services.AddHttpClient<AuthClientService>();
 
 builder.Services.AddDIServices();
 
